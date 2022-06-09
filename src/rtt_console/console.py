7370abf002ff26a7e6ed5392f60cfbeeef4e599c
Viacheslav <slava.k@ks2corp.com>
2022-06-09 13:14:19 +0300

    feat: :sparkles:  target power control via JLink

--- src/rtt_console/console.py
+++ src/rtt_console/console.py
@@ -13,7 +13,8 @@ from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 from prompt_toolkit.completion import WordCompleter
 from prompt_toolkit.patch_stdout import patch_stdout
 
-from rtt_console.default_command import COMMANDS
+from rtt_console.default_command import (CONSOLE_COMMANDS, POWER_CMD,
+                                         RECONNECT_CMD, RESET_CMD)
 from rtt_console.jlink_dongle import JLinkDongle, JLinkDongleException
 from rtt_console.version import VERSION
 
@@ -21,7 +22,7 @@ DESCRIPTION = f'RTT Console {Clr.GREEN}v{VERSION}{Clr.RESET}'
 CHIP_NAME_DEFAULT = 'STM32F407VE'
 
 cmd_queue = Queue()
-completer = WordCompleter(COMMANDS)
+completer = WordCompleter(CONSOLE_COMMANDS) # type: ignore
 
 
 def exception_handling(func: Callable):
@@ -32,6 +33,8 @@ def exception_handling(func: Callable):
             return func(self, *args, **kwargs)
         except JLinkDongleException as e:
             print(e)
+            if func.__name__ == "reconnect":
+                time.sleep(1)
             return False
 
     return wrap
@@ -64,10 +67,11 @@ def reset_target(jlink: JLinkDongle) -> bool:
     jlink.reset_target()
     return True
 
+@exception_handling
+def power_on(jlink: JLinkDongle, on:bool) -> bool:
+    jlink.power_on(on)
+    return True
 
-HELP_CMD = {'help', 'h'}
-RECONNECT_CMD = {'reconnect', 'r'}
-RESET_CMD = {'reset', 'rst'}
 
 def conole_read_input(kill_evt: Event):
     session = PromptSession()
@@ -93,30 +97,37 @@ def main():
 
     parser.add_argument(f'-s', '--speed', type=int, help='Target speed (default: auto)', required=False, default=0)
     parser.add_argument('-p', '--path', type=str, help='Path to JLink DLL', required=False, default="")
+    parser.add_argument('-pwr',
+                        '--power',
+                        help='Power on target by JLink',
+                        action=argparse.BooleanOptionalAction,
+                        required=False,
+                        default=False)
     args = parser.parse_args()
+
 
     try_to_reconnect = False
     if args.speed == 0:
         args.speed = 'auto'
-    jlink = JLinkDongle(chip_name=args.target, speed=args.speed, dll_path=args.path)
-    if connect(jlink) == False:
-        return
+    jlink = JLinkDongle(chip_name=args.target, speed=args.speed, dll_path=args.path, pwr_target=args.power)
+    try_to_reconnect = not connect(jlink)
     kill_evt = Event()
     input: Thread = Thread(target=conole_read_input, args=([kill_evt]), daemon=True)
     input.start()
     while not kill_evt.wait(0.01):
         if try_to_reconnect:
-            try_to_reconnect = False if reconnect(jlink) else True
-            time.sleep(1)
-            continue
-        if not cmd_queue.empty():
-            cmd = cmd_queue.get()
+            try_to_reconnect = not reconnect(jlink)
+        cmd = cmd_queue.get() if not cmd_queue.empty() else ""
+        if cmd in CONSOLE_COMMANDS:
             if cmd in RECONNECT_CMD:
                 try_to_reconnect = True
             elif cmd in RESET_CMD:
                 try_to_reconnect = not reset_target(jlink)
-            else:
-                try_to_reconnect = not write_cmd(jlink, cmd)
+            elif cmd in POWER_CMD:
+                on = True if "on" in cmd else False
+                power_on(jlink, on)
+        elif cmd:
+            try_to_reconnect = not write_cmd(jlink, cmd)
         rx_data = read_data(jlink)
         if rx_data:
             print(rx_data, end="")
